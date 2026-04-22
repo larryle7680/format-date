65c6de4797d1ba09b3cb5b14c30001e23e5e80fd
Larry <larryle7680@gmail.com>
2026-04-22 09:35:56 -0500

    Date Formatter finished

--- src/main/java/com/pluralsight/FormatDateApp.java
+++ src/main/java/com/pluralsight/FormatDateApp.java
@@ -1,23 +1,33 @@
 package com.pluralsight;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class FormatDateApp {
 static Scanner theScanner = new Scanner(System.in);
-    static LocalTime dateTime = LocalTime.now();
-    static LocalDate date = LocalDate.now();
+
+    static LocalDateTime dateTime = LocalDateTime.now();
+
+
     static DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MMM/yyyy");
     static DateTimeFormatter dateFormatter2 = DateTimeFormatter.ofPattern("dd-MMM-yyyy");
-    static DateTimeFormatter dateFormatter3 = DateTimeFormatter.ofPattern("E, MMM dd yyyy");
-    static DateTimeFormatter dateFormatter4 = DateTimeFormatter.ofPattern("E, MMM dd yyyy");
+    static DateTimeFormatter dateFormatter3 = DateTimeFormatter.ofPattern("EEEE, MMM dd yyyy");
+    static DateTimeFormatter dateFormatter4 = DateTimeFormatter.ofPattern("EEEE, MMM dd yyyy HH:mm");
+    static ZonedDateTime gMT = ZonedDateTime.now(ZoneId.of("GMT"));
 
     public static void main(String[] args) {
-        LocalTime dateTime = LocalTime.now();
-        LocalDate date = LocalDate.now();
+        String newDateTime = dateTime.format(dateFormatter);
+        String newDateTime1 = dateTime.format(dateFormatter2);
+        String newDateTime2 = dateTime.format(dateFormatter3);
+        String newDateTime3 = dateTime.format(dateFormatter4);
+
+        System.out.println(newDateTime);
+        System.out.println(newDateTime1);
+        System.out.println(newDateTime2);
+        System.out.println(gMT.format(dateFormatter4));
+
+
 
 
 
